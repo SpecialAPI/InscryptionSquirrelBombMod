12e83b9282de7a5a432bcb600dd8a6a25e1ec8d3
SpecialAPI <68025759+SpecialAPI@users.noreply.github.com>
2025-09-09 23:13:16 +0300

    Add the asset bundle unity project to the repository

---
+++ Assets/BigShotChallenges/Assets/Editor/BuildAssetBundles.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class BuildAssetBundles
+{
+    [MenuItem("Assets/Build AssetBundles")]
+    public static void BuildAllAssetBundles()
+    {
+        string assetBundleDirectory = "AssetBundles";
+        if (!Directory.Exists(assetBundleDirectory))
+        {
+            Directory.CreateDirectory(assetBundleDirectory);
+        }
+        try
+        {
+            BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.StrictMode, BuildTarget.StandaloneWindows);
+            DirectoryInfo d = new DirectoryInfo(assetBundleDirectory);
+            File.Delete($"{assetBundleDirectory}/AssetBundles");
+            foreach (var file in d.GetFiles("*.manifest"))
+            {
+                file.Delete();
+            }
+            foreach (var file in d.GetFiles("*.manifest.meta"))
+            {
+                file.Delete();
+            }
+            AssetDatabase.Refresh();
+            Debug.Log("<b>✔️ SUCCESSFULLY BUILDED ASSETBUNDLES ✔️</b>");
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("AN ERROR OCCURED WHILE BUILDING THE ASSETBUNDLES!\n" + e.ToString());
+        }
+    }
+}
